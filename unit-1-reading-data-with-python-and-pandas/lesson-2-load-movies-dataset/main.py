d23e911eab427ff21cf0cc72699b08b844152152
seangonzo <seanggonzales@icloud.com>
2022-09-01 13:34:53 -0700

    Finished the lecture

--- unit-1-reading-data-with-python-and-pandas/lesson-2-load-movies-dataset/main.py
+++ unit-1-reading-data-with-python-and-pandas/lesson-2-load-movies-dataset/main.py
@@ -5,3 +5,12 @@ column_names = ['color', 'director_name', 'num_critic_for_reviews', 'duration',
                 'gross', 'movie_title', 'num_user_for_reviews',	'country',
                 'cotent_rating', 'budget', 'title_year', 'imdb_score', 'genre']
 
+movies = pd.read_csv('files/movies.csv',
+                     header=None,
+                     names=column_names,
+                     sep='|',
+                     na_values=['','?','-'],
+                     skiprows=3)
+
+movies.head(10)
+
